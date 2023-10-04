37b407f0129dc35b61c1b934e05d1a303a3ab660
York Xiang <bombless@126.com>
2023-10-04 08:40:04 +0800

    Render after click

--- src/ui/ui.rs
+++ src/ui/ui.rs
@@ -33,7 +33,7 @@ pub struct State {
     vertices: Vec<f32>,
     indices: Vec<u16>,
     radius: f32,
-    count_segments: usize,
+    segments_count: usize,
     buffer: wgpu::Buffer,
     index_buffer: wgpu::Buffer,
     color: color::State,
@@ -53,12 +53,12 @@ impl State {
         rpass.set_bind_group(0, bind_group, &[]);
         rpass.set_vertex_buffer(0, self.buffer.slice(..));
         rpass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
-        if self.buffer.size() > 0 {
-            rpass.draw_indexed(0..self.num_indices_cursor(), 0, 0..1);
-            let from = self.num_indices_cursor();
-            let to = self.num_indices_paths() + self.num_indices_cursor();
-            rpass.draw_indexed(from ..to, 0, 0..1);
-        }
+
+        let index_buffer_len = self.index_buffer.size() as u32 / 2;
+        let indices_len = self.indices.len() as u32;
+
+        rpass.draw_indexed(0..indices_len.min(index_buffer_len), 0, 0..1);
+
         self.text.draw(rpass);
     }
     pub fn vertices(&self) -> &[u8] {
@@ -77,7 +77,7 @@ impl State {
     }
 
     pub fn check_buffer(&mut self, device: &Device) {
-        if self.buffer.size() == self.vertices().len() as _ {
+        if self.buffer.size() >= self.vertices().len() as u64 * 4 {
             return
         }
 
@@ -90,7 +90,7 @@ impl State {
         self.buffer = cursor_buffer;
     }
     pub fn check_index_buffer(&mut self, device: &Device) {
-        if self.index_buffer.size() == self.indices().len() as _ {
+        if self.index_buffer.size() >= self.indices().len() as u64 * 2 {
             return
         }
 
@@ -103,10 +103,10 @@ impl State {
         self.index_buffer = index_buffer;
     }
     pub fn num_indices_cursor(&self) -> u32 {
-        self.count_segments as u32 * 3
+        self.segments_count as u32 * 3
     }
     pub fn num_indices_paths(&self) -> u32 {
-        self.indices.len() as u32 - self.num_indices_cursor()
+        self.segments_count as u32 * 3 * self.points.len() as u32
     }
     pub fn color(&self) -> &color::State {
         &self.color
@@ -119,6 +119,7 @@ impl State {
     }
     pub fn push_point(&mut self) {
         self.points.last_mut().unwrap().push(self.cursor);
+        self.update_points();
     }
     pub fn new_path(&mut self) {
         if self.points.last().unwrap().is_empty() {
@@ -127,58 +128,81 @@ impl State {
         self.points.push(Vec::new());
     }
     pub fn delete_path(&mut self) {
-        std::mem::take(self.points.last_mut().unwrap());
+        if self.points.len() == 1 {
+            self.points[0].truncate(0);
+        } else {
+            self.points.pop();
+        }
+        self.update_points();
     }
+
+    fn fill_buffer(vertices: &mut Vec<f32>, indices: &mut Vec<u16>, origin: u16,
+                   factor: f32, radius: f32, x: f32, y: f32, count_segments: u32)
+    {
+        vertices.push(x);
+        vertices.push(y);
+        for i in 0 .. count_segments {
+            let p1 = i as f32 * factor;
+            indices.push(origin);
+            indices.push(origin + i as u16 * 2 + 1);
+            indices.push(origin + i as u16 * 2 + 2);
+            let offset_x1 = p1.sin() * radius;
+            let offset_y1 = p1.cos() * radius;
+            vertices.push(offset_x1 + x);
+            vertices.push(offset_y1 + y);
+            let p2 = factor * (i + 1) as f32;
+            let offset_x2 = p2.sin() * radius;
+            let offset_y2 = p2.cos() * radius;
+            vertices.push(offset_x2 + x);
+            vertices.push(offset_y2 + y);
+        }
+    }
+
     pub fn update_cursor(&mut self, x: f32, y: f32)  {
-        let mut vertices = vec![];
+        self.cursor = (x, y);
 
-        let mut indices = vec![];
-
-        let count_segments = self.count_segments as _;
+        let count_segments = self.segments_count as _;
         let radius = self.radius;
 
-        fn fill_buffer(vertices: &mut Vec<f32>, indices: &mut Vec<u16>, origin: u16,
-                       factor: f32, radius: f32, x: f32, y: f32, count_segments: u32)
-        {
-            vertices.push(x);
-            vertices.push(y);
-            for i in 0 .. count_segments {
-                let p1 = i as f32 * factor;
-                indices.push(origin);
-                indices.push(origin + i as u16 * 2 + 1);
-                indices.push(origin + i as u16 * 2 + 2);
-                let offset_x1 = p1.sin() * radius;
-                let offset_y1 = p1.cos() * radius;
-                vertices.push(offset_x1 + x);
-                vertices.push(offset_y1 + y);
-                let p2 = factor * (i + 1) as f32;
-                let offset_x2 = p2.sin() * radius;
-                let offset_y2 = p2.cos() * radius;
-                vertices.push(offset_x2 + x);
-                vertices.push(offset_y2 + y);
-            }
+        if self.indices.is_empty() {
+            Self::fill_buffer(&mut self.vertices, &mut self.indices, 0,
+                              2.0 * std::f32::consts::PI / count_segments as f32, radius, x, y, count_segments);
+
+        } else {
+
+            let mut vertices = Vec::new();
+            let mut indices = Vec::new();
+            Self::fill_buffer(&mut vertices, &mut indices, 0,
+                              2.0 * std::f32::consts::PI / count_segments as f32, radius, x, y, count_segments);
+            self.vertices[..vertices.len()].copy_from_slice(&vertices);
+            self.indices[..indices.len()].copy_from_slice(&indices);
+        }
+    }
+
+    pub fn update_points(&mut self)  {
+        if self.indices.is_empty() {
+            return;
         }
 
-        let mut count = 0;
-        fill_buffer(&mut vertices, &mut indices, count,
-                    2.0 * std::f32::consts::PI / count_segments as f32, radius, x, y, count_segments);
+        let count_segments = self.segments_count as _;
+        let radius = self.radius;
 
+        let mut count = 0;
+
+        self.vertices.truncate(self.segments_count * 4 + 2);
+        self.indices.truncate(self.segments_count * 3);
 
         for segment in &self.points {
             for &(x, y) in segment {
                 count += count_segments as u16 * 2 + 1;
-                fill_buffer(&mut vertices, &mut indices, count,
-                            2.0 * std::f32::consts::PI / count_segments as f32, radius, x, y, count_segments);
+                Self::fill_buffer(&mut self.vertices, &mut self.indices, count,
+                                  2.0 * std::f32::consts::PI / count_segments as f32, radius, x, y, count_segments);
             }
         }
 
-        if indices.len() % 2 == 1 {
-            indices.push(0);
+        if self.indices.len() % 2 == 1 {
+            self.indices.push(0);
         }
-
-        self.cursor = (x, y);
-        self.vertices = vertices;
-        self.indices = indices;
     }
     pub fn new(app: &AppSurface) -> Self {
 
@@ -214,7 +238,7 @@ impl State {
             vertices: Vec::new(),
             indices: Vec::new(),
             radius,
-            count_segments,
+            segments_count: count_segments,
             buffer: cursor_buffer,
             index_buffer: cursor_index_buffer, text, color,
         }
