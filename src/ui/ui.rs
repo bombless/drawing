5b1a8dcf0e7d36d5b04cabfead43ebc12c6dea0f
York Xiang <bombless@126.com>
2023-10-04 16:51:10 +0800

    Refactor: extract `draw_line()`

--- src/ui/ui.rs
+++ src/ui/ui.rs
@@ -236,6 +236,8 @@ impl State {
     }
 
     pub fn update_points(&mut self)  {
+        use super::utils::draw_line;
+
         if self.indices.is_empty() {
             return;
         }
@@ -256,38 +258,6 @@ impl State {
             }
         }
 
-        fn get_radian(p1: (f32, f32), p2: (f32, f32)) -> f32 {
-            ((p2.0 - p1.0) / (p2.1 - p1.1)).atan()
-        }
-        
-        fn draw_line(p1: (f32, f32), p2: (f32, f32), vertices: &mut Vec<f32>, indices: &mut Vec<u16>,
-                     radius: f32, origin: u16)
-        {
-
-            let radian = get_radian(p1, p2);
-            let offset_x1 = (radian + std::f32::consts::PI / 2.0).sin() * radius;
-            let offset_x2 = (radian - std::f32::consts::PI / 2.0).sin() * radius;
-            let offset_y1 = (radian + std::f32::consts::PI / 2.0).cos() * radius;
-            let offset_y2 = (radian - std::f32::consts::PI / 2.0).cos() * radius;
-
-            vertices.push(offset_x1 + p1.0);
-            vertices.push(offset_y1 + p1.1);
-            vertices.push(offset_x2 + p1.0);
-            vertices.push(offset_y2 + p1.1);
-            vertices.push(offset_x1 + p2.0);
-            vertices.push(offset_y1 + p2.1);
-            vertices.push(offset_x2 + p2.0);
-            vertices.push(offset_y2 + p2.1);
-
-            indices.push(origin);
-            indices.push(origin + 1);
-            indices.push(origin + 2);
-
-            indices.push(origin + 1);
-            indices.push(origin + 2);
-            indices.push(origin + 3);
-            
-        }
 
         for (i, segment) in self.points.iter().enumerate() {
             if segment.is_empty() {
